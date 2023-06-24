051c798f076822732667fb471066286b5c1d27a1
Tomasz Goldmann <tgoldmann3b1@gmail.com>
2023-06-24 17:31:29 +0200

    dropdown prices

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let totalArray = []
 
 const values = {
     products: 0.5,
-    orders: 0.25,
+    orders: 0.5,
     package: {
         basic: 0,
         professional: 25,
@@ -34,6 +34,17 @@ function handlePackage(event) {
     dropDownItems.style.color = 'black'
     listItems[2].style.display = 'block'
     listItems[2].children[1].innerText = event.target.innerText
+    if (event.target.innerText === 'Basic'){
+        listItems[2].children[2].innerText = `$${values.package.basic}`
+        totalArray[4] = values.package.basic
+    } else if (event.target.innerText === 'Professional'){
+        listItems[2].children[2].innerText = `$${values.package.professional}`
+        totalArray[4] = values.package.professional
+    } else {
+        console.log(values.package.premium)
+        listItems[2].children[2].innerText = `$${values.package.premium}`
+        totalArray[4] = values.package.premium
+    }
 }
 
 function handleChange(event) {
